f51fba4a0144ff449fc7aa61b4d71f7b84e4aa4a
mobius <mobius_os@163.com>
2026-08-13 15:13:31 +0000

    Refine bright publication visuals

--- mobius/extension/mobius-home-light/frontend/dist/bootstrap.js
+++ mobius/extension/mobius-home-light/frontend/dist/bootstrap.js
@@ -9,14 +9,10 @@
       .replace('<html lang="zh-CN">', '<html lang="zh-CN" class="mobius-light">')
       .replace('<title>莫比乌斯 · Publication</title>', '<title>莫比乌斯 · 明亮版</title>')
       .replace(
-        './assets/mobius-synergy.svg',
-        '/extension/mobius-home-light/mobius-synergy-light.svg?v=1'
-      )
-      .replace(
         '<link rel="stylesheet" href="./styles.css" />',
         '<base href="/extension/mobius-home/" />\n' +
         '  <link rel="stylesheet" href="/extension/mobius-home/styles.css" />\n' +
-        '  <link rel="stylesheet" href="/extension/mobius-home-light/light.css?v=3" />'
+        '  <link rel="stylesheet" href="/extension/mobius-home-light/light.css?v=2" />'
       )
       .replace(
         '<script type="module" src="./main.js"></script>',
